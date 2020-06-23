1089fda9a20c6173ac7806cc035530c755a60139
amjadjirawi <amjadjearawy@gmail.com>
2020-06-23 15:06:19 +0300

    new

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+
+alert ('hello');
+console.log('Matar')
+var a ='amjad'
+var b = '25'
+console.log('name=',a)
+//
+// var today= new Date();
+// var hourNow = today.getHours();
+// var greeting;
+// if (hourNow > 18) {
+// greeting= 'Good evening!';
+// else if (hourNow > 12) {
+// greeting = ' Good afternoon!';
+// else if (hourNow > 0) {
+// greeting = 'Good morni ng!';
+// else {
+// greeting = 'Welcome! ' ;
+// document .write( ' <h3>' +greeting + ' </ h3> '); 
