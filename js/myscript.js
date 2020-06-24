ddd7c6e15a75132e8d923719acc57b3a246acfd1
amjadjirawi <amjadjearawy@gmail.com>
2020-06-24 06:25:01 +0300

    Updates

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,24 @@
+'use strict';
+alert ('Welcom');
+var txt;
+var r = confirm("Are you a Matar volunteer?");
+if (r == true) {
+  txt = "Thank You for your efforts";
+} else {
+  txt = "see you next time";
+}
+document.write('<h3>' + txt + '</h3>');
 
-alert ('hello');
-console.log('Matar')
-var a ='amjad'
-var b = '25'
-console.log('name=',a)
-//
-// var today= new Date();
-// var hourNow = today.getHours();
-// var greeting;
-// if (hourNow > 18) {
-// greeting= 'Good evening!';
-// else if (hourNow > 12) {
-// greeting = ' Good afternoon!';
-// else if (hourNow > 0) {
-// greeting = 'Good morni ng!';
-// else {
-// greeting = 'Welcome! ' ;
-// document .write( ' <h3>' +greeting + ' </ h3> '); 
+var hours = prompt('Please enter your tatal volunteering hours');
+var volunteer;
+if (hours > 50 && hours <= 100) {
+      volunteer = 'You are Great';
+    } else if (hours > 20 && hours <= 50) {
+        volunteer = 'you are good but need more';
+    } else if (hours >= 0 && hours <= 20 ) {
+        volunteer  = 'Do more efort to help others';
+    } else {
+        volunteer  = 'Something went wrong!';
+    }
+    document.write('<h3>' + volunteer + '</h3>');
+    
