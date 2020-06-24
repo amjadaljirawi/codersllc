0552f0db799900caf0521a453d1ace9fe5420450
amjadjirawi <amjadjearawy@gmail.com>
2020-06-24 15:47:55 +0300

    updates

--- js/myscript.js
+++ js/myscript.js
@@ -2,39 +2,76 @@
 alert('Welcom');
 
 
+
 function askForDonation() {
   var mas;
+  var how;
+  var why;
   var money = confirm('Can You help us to help others by your generous donation?')
  
-    if (money == true) {
-      mas = "Thank You for your efforts";
-    } else {
-      mas = "Whyyyy";
+  if (money == true){
+    how = prompt('how much please chose on of these unmbers 1 5 10 20 50');
+    while (how !== '1' && how !== '5'&& how !== '10'&& how !== '20'&& how !== '50') {
+      how = prompt('how much please chose on of these unmbers 1 5 10 20 50');
+      }
+    mas = "Thank You for your efforts";
+} else {
+  why = prompt('Are you sure? yes or no')
+  while (why !== 'yes' && why !== 'no') {
+    why = prompt('Are you sure? yes or no')
     }
-    document.write('<img class="img2" src="img/29027219_1646899498758564_1278825000555511808_o.jpg"> ' );
-    
-  
-  
-    var txt;
-    var r = confirm("Are you a Matar volunteer?");
-    if (r == true) {
-      txt = "Thank You for your efforts";
-    } else {
-      txt = "see you next time";
-   }
-   document.write('<h3>' + txt + '</h3>');
-
-    var hours = prompt('Please enter your tatal volunteering hours');
-    var volunteer;
-    if (hours > 50 && hours <= 100) {
-      volunteer = 'You are Great';
-    } else if (hours > 20 && hours <= 50) {
-      volunteer = 'you are good but need more';
-    } else if (hours >= 0 && hours <= 20) {
-      volunteer = 'Do more efort to help others';
-    } else {
-      volunteer = 'Something went wrong!';
-    }
-    document.write('<h3>' + volunteer + '</h3>');
+  mas = "Whyyyy";
 }
+document.write('<img class="img2" src="img/29027219_1646899498758564_1278825000555511808_o.jpg"> ' + mas );
+
+
+
+var txt;
+var r = confirm("Are you a Matar volunteer?");
+if (r == true) {
+  txt = "Thank You for your efforts";
+} else {
+  txt = "see you next time";
+}
+document.write('<h3>' + txt + '</h3>');
+
+var hours = prompt('Please enter your tatal volunteering hours');
+var volunteer;
+if (hours > 50 && hours <= 100) {
+  volunteer = 'You are Great';
+} else if (hours > 20 && hours <= 50) {
+  volunteer = 'you are good but need more';
+} else if (hours >= 0 && hours <= 20) {
+  volunteer = 'Do more efort to help others';
+} else {
+  volunteer = 'Something went wrong!';
+}
+document.write('<h3>' + volunteer + '</h3>');
+for (var i = 0; i < hours; i++) {
+  document.write('<img class="img3" src="img/kiss.jpg">');
+}
+
+  }
 askForDonation()
+
+
+
+// var userWant = prompt('what do you want to order house or hotel ?');
+// //userWant = hotel
+// while (userWant !== 'house' && userWant !== 'hotel') {
+//   // T && F >>> F
+//   userWant = prompt('what do you want to order house or hotel ?');
+// }
+// var orderNumbers = prompt('how many items do you want to order?');
+// //userWant = hotel
+// //orderNumbers = 6
+// // i = 2
+//  {
+//   if (userWant === 'house') {
+//     //code
+//     document.write('<img id="block" src="images/house.png">');
+//   } else if (userWant === 'hotel') {
+//     //code
+//     document.write('<p><img src="images/hotel.png"></p>');
+//   }
+// }
